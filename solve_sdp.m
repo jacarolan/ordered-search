e458d4450a031fceb154f5b1e44c43d01b52575c
mkdeak <matt@mpro.local>
2024-06-30 23:07:04 -0700

    refactor: moved utilities to separate files

--- solve_sdp.m
+++ solve_sdp.m
@@ -1,7 +1,7 @@
 yalmip('clear')
 
 q = 3;
-N = 200;
+N = 4;
 
 
 Rs = zeros(N, N, N);
@@ -14,8 +14,6 @@ tr = @(M, i) trace(Rs(:,:,i) * M);
 
 % Setting up the SDP
 m = N/2;
-diag_idx = (1:m+1:m^2).' + (0:q-1)*m^2;
-
 I = eye(m);
 J = fliplr(eye(m));
 E = ones(m, m);
@@ -23,25 +21,15 @@ E = ones(m, m);
 As = sdpvar(m, m, q+1); 
 Bs = sdpvar(m, m, q+1, 'full');
 
-function prod = pagemtimesLeft(M, Pgs)
-    row_cnt = size(Pgs, 1);
-    pg_cnt = size(Pgs, 3);
-    prod = reshape(M * reshape(Pgs, row_cnt, []), row_cnt, row_cnt, pg_cnt);
-end
-
-function prod = pagemtimesRight(Pgs, M)
-    prod = pagetranspose(pagemtimesLeft(transpose(M), pagetranspose(Pgs)));
-end
-
 Constraints = [
     As(:,:,1) == E/N,... 
     Bs(:,:,1) == E/N,...
     As(:,:,q+1) == eye(m)/N,...
     Bs(:,:,q+1) == zeros(m),...
-    sum(As(diag_idx), 1) == 1/2,... 
-    pagemtimesLeft(J, Bs) == pagetranspose(pagemtimesLeft(J, Bs)),...
-    pagemtimesRight((As + Bs), J) >= 0,...
-    pagemtimesRight((As - Bs), J) >= 0,...
+    pagetraces(As) == 1/2,... 
+    pagemtimes_left(J, Bs) == pagetranspose(pagemtimes_left(J, Bs)),...
+    pagemtimes_right((As + Bs), J) >= 0,...
+    pagemtimes_right((As - Bs), J) >= 0
 ];
 
 % TODO vectorize these as well
